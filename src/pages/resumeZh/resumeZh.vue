7f66d905fef9afd7c7623d332459162d000a694b
刘阳 <liuyang@liuyangs-MacBook-Pro.local>
2024-10-27 16:27:19 +0800

    update

--- src/pages/resumeZh/resumeZh.vue
+++ src/pages/resumeZh/resumeZh.vue
@@ -50,7 +50,7 @@ import Avatar from '../../assets/avatar.jpg'
         <!-- body start -->
         <h2>项目经历</h2>
         <n-timeline>
-            <n-timeline-item type="success" title="AI 软件开发工程师 | 华为" content="使用 RAG 帮助团队提高问题定位效率，从 es、mysql 以及其他系统中进行搜索后针对问题进行重排序并总结。">
+            <n-timeline-item type="success" title="AI 软件开发工程师 | 华为" content="使用 RAG 帮助团队提高问题定位效率，从 es、mysql 以及其他系统中进行搜索后针对问题进行重排序并总结。其中多路召回重排序方法在公司内部申请专利成功，问题搜索准确率由50%提升至90%，获“数据通信研发部长奖-维护效率及能力提升奖”。">
                 <template #footer>
                     2024.01 - Now
                     <n-space>
@@ -66,7 +66,7 @@ import Avatar from '../../assets/avatar.jpg'
                     </n-space>
                 </template>
             </n-timeline-item>
-            <n-timeline-item title="嵌入式系统开发工程师 | 华为" content="开发新版本需求，定位旧版本问题并开发补丁进行修复。作为 CIE，负责维护版本构建流水线以及补丁构建网站。工作之余，开发了多个应用提高团队开发与问题定位效率。">
+            <n-timeline-item title="软件开发工程师 | 华为" content="主要负责系统支撑工作，开发新版本需求，维护网上版本，出补丁解决发布版本bug，责任田主要为控制器上线、设备登录、补丁机制。作为 CIE，负责维护版本构建与补丁构建网站，提高版本构建效率 20% 以上，开发人员单次补丁构建时间由 5min 减少到 1min，大幅提升工作效率，自动化补丁归档，规范化补丁构建流程，提高补丁发布效率 30% 以上。作为 redis 的 PMC，负责 redis 归一与升级工作，获“redis 定制归一攻关激励团队奖”。工作之余，开发了多个应用提高团队开发与问题定位效率。">
                 <template #footer>
                     2021.09 - 2023.12
                     <n-space>
@@ -81,7 +81,7 @@ import Avatar from '../../assets/avatar.jpg'
                     </n-space>
                 </template>
             </n-timeline-item>
-            <n-timeline-item title="Java 开发工程师 | 睿企科技" content="在公司开发的 ETL 平台开发模块处理和分析大数据。设计、开发并维护多个应用用于展示分析过的数据。">
+            <n-timeline-item title="Java 开发工程师 | 睿企科技" content="使用公司算法部门提供的 NLP 等能力，编写大数据标准化处理模块，维护标准化处理流程，质态监管、热词雷达、自动报告等各个应用的后端设计与实现，对接其他公司，偶尔帮忙改改前端 bug & 写一些简单的管理页面。">
                 <template #footer>
                     2020.05 - 2021.07
                     <n-space>
@@ -105,8 +105,9 @@ import Avatar from '../../assets/avatar.jpg'
                         <n-tag :bordered="false" round size="small">SpringMVC</n-tag>
                         <n-tag :bordered="false" round size="small">Oracle</n-tag>
                         <n-tag :bordered="false" round size="small">Electron</n-tag>
-                        <n-tag :bordered="false" round size="small">Aurelia</n-tag>
+                        <n-tag :bordered="false" round size="small">Mqtt</n-tag>
                         <n-tag :bordered="false" round size="small">SVN</n-tag>
+                        <n-tag :bordered="false" round size="small">Kafka</n-tag>
                     </n-space>
                 </template>
             </n-timeline-item>
@@ -114,7 +115,7 @@ import Avatar from '../../assets/avatar.jpg'
         <n-divider />
         <h2>教育</h2>
         <n-timeline>
-            <n-timeline-item title="软件工程-本科 | 电子科技大学" content="学习的主要课程：软件工程、计算机组成原理、计算机网络、嵌入式系统、数据结构预算法、机器视觉算法与应用、综合课程设计（人脸识别）。获得了英语四级证书和人民三等奖学金。">
+            <n-timeline-item title="软件工程-本科 | 电子科技大学" content="学习的主要课程：软件工程、计算机组成原理、计算机网络、嵌入式系统、数据结构与算法、机器视觉算法与应用、综合课程设计（人脸识别）。获得了英语四级证书和人民三等奖学金。">
                 <template #footer>
                     2014.09 - 2018.06
                 </template>
