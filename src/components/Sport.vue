7c0301eb4354dd664cf509e24191066bf866cee6
刘阳 <liuyang@liuyangs-MacBook-Pro.local>
2024-11-06 22:01:30 +0800

    update sport

--- src/components/Sport.vue
+++ src/components/Sport.vue
@@ -28,7 +28,7 @@ const option = {
   xAxis: {
     type: 'category',
     boundaryGap: false,
-    data: ['11.01', '11.02', '11.03', '11.04', '11.05'],
+    data: ['11.01', '11.02', '11.03', '11.04', '11.05', '11.06'],
     axisLabel: {
         show: true,
         textStyle: {
@@ -51,17 +51,17 @@ const option = {
     {
       name: 'Calories',
       type: 'line',
-      data: [408, 720, 269, 695, 665]
+      data: [408, 720, 269, 695, 665, 819]
     },
     {
       name: 'Weight',
       type: 'line',
-      data: [76, 76, 76, 76, 75]
+      data: [76.0, 76.0, 76.0, 76.0, 75.1, 75.5]
     },
     {
       name: 'Exercise',
       type: 'line',
-      data: [34, 81, 8, 71, 66]
+      data: [34, 81, 8, 71, 66, 94]
     }
   ]
 };
