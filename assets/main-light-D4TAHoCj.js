b8b6e1b46eabcf1188ca018cc4ddebcaa408481d
42tr <34233822+42tr@users.noreply.github.com>
2024-11-16 15:10:17 +0000

    Deploying to gh-pages from  @ 6b4d7b250042502047e889ff6f73744e40450f99 🚀

---
+++ assets/main-light-D4TAHoCj.js
@@ -0,0 +1,1 @@
+import{d as i}from"./main-light-jU1J2zW8.js";function n(o){const{textColor1:t,dividerColor:r,fontWeightStrong:e}=o;return{textColor:t,color:r,fontWeight:e}}const s={name:"Divider",common:i,self:n};export{s as d,n as s};
